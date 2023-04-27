dba4dce239d03dcd7b6bc7d5eb139406013e490a
miouzora <alexandre.franquet@epitech.eu>
2023-04-27 17:40:39 +0200

    feat(MathsUtils): add GetReflectedRay with ray

--- src/Maths/MathsUtils.hpp
+++ src/Maths/MathsUtils.hpp
@@ -8,15 +8,40 @@
 #pragma once
 
 #include "Vector.hpp"
+#include "Ray.hpp"
 #include <cmath>
 
 namespace Maths {
+    /**
+     * @brief A class that contains some useful maths functions
+     */
     class MathsUtils {
         public:
+            /**
+             * @brief Get the Reflected Ray object
+             *
+             * @param normal
+             * @param incident
+             * @return Maths::Vector
+             */
             static Maths::Vector GetReflectedRay(const Maths::Vector &normal,
                 const Maths::Vector &incident)
             {
                 return (((normal * incident) * normal) * 2 - incident);
+            }
+
+            /**
+             * @brief Get the Reflected Ray object
+             *
+             * @param normal
+             * @param incident
+             * @return Maths::Ray
+             */
+            static Maths::Ray GetReflectedRay(const Maths::Vector &normal,
+                const Maths::Ray &incident)
+            {
+                return (Maths::Ray(incident._origin,
+                    GetReflectedRay(normal, incident._direction)));
             }
     };
 }
