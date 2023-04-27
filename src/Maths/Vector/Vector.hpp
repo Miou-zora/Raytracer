ac7380db34291ef8be1ba0dd06ddb5e3569dbabb
miouzora <alexandre.franquet@epitech.eu>
2023-04-27 16:33:35 +0200

    doc(Maths/Vector): add vector doc to vector class

--- src/Maths/Vector/Vector.hpp
+++ src/Maths/Vector/Vector.hpp
@@ -10,37 +10,223 @@
 #include <iostream>
 
 namespace Maths {
+    /**
+     * @brief A vector in 3D space
+     */
     class Vector {
         public:
+            /**
+             * @brief Construct a new Vector object
+             */
             Vector(void) = default;
+
+            /**
+             * @brief Construct a new Vector object
+             *
+             * @param x
+             * @param y
+             * @param z
+             */
             Vector(double x, double y, double z);
+
+            /**
+             * @brief Construct a new Vector object
+             *
+             * @param other
+             */
             Vector(const Vector &other);
+
+            /**
+             * @brief Construct a new Vector object
+             *
+             * @param other
+             */
             Vector(const Vector &&other);
+
+            /**
+             * @brief Destroy the Vector object
+             */
             ~Vector() = default;
 
+            /**
+             * @brief Copy the Vector object with copy operator
+             *
+             * @param other
+             * @return Vector&
+             */
             Vector &operator= (const Vector &other);
+
+            /**
+             * @brief Copy the Vector object with move operator
+             *
+             * @param other
+             * @return Vector&
+             */
             Vector &operator= (const Vector &&other);
+
+            /**
+             * @brief Add two vectors
+             *
+             * @param other
+             * @return Vector
+             */
             Vector operator+ (const Vector &other);
+
+            /**
+             * @brief Add two vectors
+             *
+             * @param other
+             * @return Vector&
+             */
             Vector &operator+= (const Vector &other);
+
+            /**
+             * @brief Subtract two vectors
+             *
+             * @param other
+             * @return Vector
+             */
             Vector operator- (const Vector &other);
+
+            /**
+             * @brief Subtract two vectors
+             *
+             * @param other
+             * @return Vector&
+             */
             Vector &operator-= (const Vector &other);
+
+            /**
+             * @brief Multiply two vectors
+             *
+             * @param other
+             * @return Vector
+             */
             Vector operator* (const Vector &other);
+
+            /**
+             * @brief Multiply two vectors
+             *
+             * @param other
+             * @return Vector&
+             */
             Vector &operator*= (const Vector &other);
+
+            /**
+             * @brief Divide two vectors
+             *
+             * @param other
+             * @return Vector
+             */
             Vector operator/ (const Vector &other);
+
+            /**
+             * @brief Divide two vectors
+             *
+             * @param other
+             * @return Vector&
+             */
             Vector &operator/= (const Vector &other);
+
+            /**
+             * @brief Add a value to a vector
+             *
+             * @param value
+             * @return Vector
+             */
             Vector operator+ (double value);
+
+            /**
+             * @brief Add a value to a vector
+             *
+             * @param value
+             * @return Vector&
+             */
             Vector &operator+= (double value);
+
+            /**
+             * @brief Subtract a value to a vector
+             *
+             * @param value
+             * @return Vector
+             */
             Vector operator- (double value);
+
+            /**
+             * @brief Subtract a value to a vector
+             *
+             * @param value
+             * @return Vector&
+             */
             Vector &operator-= (double value);
+
+            /**
+             * @brief Multiply a vector by a value
+             *
+             * @param value
+             * @return Vector
+             */
             Vector operator* (double value);
+
+            /**
+             * @brief Multiply a vector by a value
+             *
+             * @param value
+             * @return Vector&
+             */
             Vector &operator*= (double value);
+
+            /**
+             * @brief Divide a vector by a value
+             *
+             * @param value
+             * @return Vector
+             */
             Vector operator/ (double value);
+
+            /**
+             * @brief Divide a vector by a value
+             *
+             * @param value
+             * @return Vector&
+             */
             Vector &operator/= (double value);
 
+            /**
+             * @brief Make the dot product of two vectors
+             *
+             * @param other The other vector
+             * @return double The dot product
+             */
             double dot(const Vector &other) const;
+
+            /**
+             * @brief Make the cross product of two vectors
+             *
+             * @param other The other vector
+             * @return Vector The cross product
+             */
             Vector cross(const Vector &other) const;
+
+            /**
+             * @brief Get the length of the vector
+             *
+             * @return double The length
+             */
             double length(void) const;
+
+            /**
+             * @brief Normalize the vector
+             *
+             * @return Vector& The normalized vector
+             */
             Vector &normalize(void);
+
+            /**
+             * @brief Get the normalized vector
+             *
+             * @return Vector The normalized vector
+             */
             Vector normalized(void) const;
 
             double _x;
