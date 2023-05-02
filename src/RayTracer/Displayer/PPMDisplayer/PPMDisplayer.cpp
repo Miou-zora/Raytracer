f08aec0662b1b48aeeeb1c91ee1f877ba6b6f2d3
quentin <Quentin.brejoin@gmail.com>
2023-05-02 14:34:26 +0200

    feat(core): add display function (#28)
    * feat(core): add display function
    
    * fix(PPM::Display): attributs names
    
    * feat(PPMDisplay): add tests
    
    * update(Cmake)

--- src/RayTracer/Displayer/PPMDisplayer/PPMDisplayer.cpp
+++ src/RayTracer/Displayer/PPMDisplayer/PPMDisplayer.cpp
@@ -9,5 +9,13 @@
 
 void RayTracer::PPMDisplayer::display(const RayTracer::Frame &frame)
 {
-    (void)frame;
+    std::cout << "P3" << std::endl;
+    std::cout << frame.getWidth() << " " << frame.getHeight() << std::endl;
+    std::cout << "255" << std::endl;
+    for (std::size_t i = 0; i < frame.getHeight(); i++) {
+        for (std::size_t j = 0; j < frame.getWidth(); j++) {
+            RayTracer::RGBAColor pixel = frame.getPixel(std::make_pair(i, j));
+            std::cout << pixel.getRed() << " " << pixel.getGreen() << " " << pixel.getBlue() << std::endl;
+        }
+    }
 }
--- src/RayTracer/Frame/Frame.hpp
+++ src/RayTracer/Frame/Frame.hpp
@@ -14,16 +14,17 @@
 namespace RayTracer {
     class Frame {
         public:
-            Frame(std::size_t width, std::size_t height): _width(width), _height(height) {};
+            Frame(std::size_t width, std::size_t height): _width(width), _height(height) { _rect = std::vector<std::vector<RayTracer::RGBAColor>>(height, std::vector<RayTracer::RGBAColor>(width, RayTracer::RGBAColor(0, 0, 0)));};
             ~Frame() = default;
 
             void setPixel(const std::pair<std::size_t, std::size_t> &pos, const RayTracer::RGBAColor &color);
+            const RayTracer::RGBAColor &getPixel(const std::pair<std::size_t, std::size_t> &pos) const { return _rect[pos.first][pos.second]; };
 
-            const std::vector<std::vector<RayTracer::RGBAColor>> &getFrame(void) const;
-
+            std::size_t getWidth(void) const { return _width; };
+            std::size_t getHeight(void) const { return _height; };
         private:
             std::size_t _width;
             std::size_t _height;
-            std::vector<std::vector<RayTracer::RGBAColor>> _frame; // 3D array of vertices (pixels)
+            std::vector<std::vector<RayTracer::RGBAColor>> _rect; // 2D array of vertices (pixels)
     };
 }
