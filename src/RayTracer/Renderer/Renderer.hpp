d09b66a435c6e4280e48a29939f2601d9441a192
quentin <Quentin.brejoin@gmail.com>
2023-05-03 08:59:29 +0200

    feat(Renderer): add default renderer without taking care of distances… (#29)
    * feat(Plane): add plane class
    
    * feat(Renderer): add default renderer without taking care of distances and lights
    
    * feat(Plane): add hit method
    
    * feat(HitRecord): add distance to hitrecord
    
    * tests(Plane): add tests for Plane class
    
    * feat(Renderer): add distances to renderer
    
    * fix(Renderer): rm Renderer
    
    * fix(AFTER REVIEW)
    
    ---------
    
    Co-authored-by: miouzora <alexandre.franquet@epitech.eu>
    Co-authored-by: Miouzora <91665686+Miou-zora@users.noreply.github.com>

--- src/RayTracer/Renderer/Renderer.hpp
+++ src/RayTracer/Renderer/Renderer.hpp
@@ -12,12 +12,14 @@
 namespace RayTracer {
     class Renderer : virtual public IRenderer{
         public:
-            Renderer(void);
-            ~Renderer();
+            Renderer(void) = default;
+            ~Renderer() = default;
 
             void render(const RayTracer::AScene &scene, RayTracer::Frame &frame);
 
         protected:
         private:
+            RGBAColor castRay(const AScene &scene, const Maths::Ray &ray) const;
+            RayTracer::HitRecord getClosestHit(const std::vector<RayTracer::HitRecord> &records) const;
     };
 }
