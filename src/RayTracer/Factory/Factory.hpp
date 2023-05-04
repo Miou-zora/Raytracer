0a916961236762da382152778f2d595515858fc0
lhay <lylian.hay@epitech.eu>
2023-05-04 16:43:45 +0200

    feat(factory): create the shapesmap for the factory and the create lightmap but just none

--- src/RayTracer/Factory/Factory.hpp
+++ src/RayTracer/Factory/Factory.hpp
@@ -10,7 +10,11 @@
 #include <string>
 #include "ILight.hpp"
 #include "IShape.hpp"
+#include <map>
 #include <memory>
+#include <functional>
+#include "Sphere.hpp"
+#include "Plane.hpp"
 
 namespace RayTracer {
     class Factory {
@@ -23,5 +27,12 @@ namespace RayTracer {
 
         protected:
         private:
+            std::map<std::string, std::function<std::shared_ptr<RayTracer::IShape>(const std::string&)>> _shapeMap = {
+               {"Sphere", [](const std::string& name){ return std::make_shared<RayTracer::Sphere>(name);}},
+               {"Plane", [](const std::string& name){ return std::make_shared<RayTracer::Plane>(name);}},
+            };
+            std::map<std::string, std::function<std::shared_ptr<RayTracer::ILight>(const std::string&)>> _lightMap = {
+            //    {"PointLight", [](const std::string& name){ return std::make_shared<RayTracer::PointLight>(name);}},
+            };
     };
 }
