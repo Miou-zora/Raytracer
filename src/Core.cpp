f78ae7a8cd3dfd785b371023eb62192f12315f3d
queng <quentin.brejoin@gmail.com>
2023-05-05 12:37:16 +0200

    fix(Cmake): libconfig

--- src/Core.cpp
+++ src/Core.cpp
@@ -14,19 +14,20 @@ void RayTracer::Core::run(void)
 
 void RayTracer::Core::setScene(std::string scenePath)
 {
-    libconfig::Config cfg;
+    (void)scenePath;
+    // libconfig::Config cfg;
 
-    if (scenePath.empty())
-        throw std::invalid_argument("Scene path is empty");
-    try {
-        cfg.readFile(scenePath.c_str());
-    } catch (const libconfig::FileIOException &fioex) {
-        throw std::invalid_argument("I/O error while reading file.");
-    }
-    catch (const libconfig::ParseException &pex) {
-        throw std::invalid_argument("Parse error at " + std::to_string(pex.getLine()) + ": " + pex.getError());
-    }
-    _scenePath = scenePath;
+    // if (scenePath.empty())
+    //     throw std::invalid_argument("Scene path is empty");
+    // try {
+    //     cfg.readFile(scenePath.c_str());
+    // } catch (const libconfig::FileIOException &fioex) {
+    //     throw std::invalid_argument("I/O error while reading file.");
+    // }
+    // catch (const libconfig::ParseException &pex) {
+    //     throw std::invalid_argument("Parse error at " + std::to_string(pex.getLine()) + ": " + pex.getError());
+    // }
+    // _scenePath = scenePath;
 }
 
 void RayTracer::Core::buildScene(void)
