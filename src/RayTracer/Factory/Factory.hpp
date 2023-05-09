f694f38446b5865d65c67dfa87d89b59d4db1b16
Lylian Hay <91667138+lhay9@users.noreply.github.com>
2023-05-09 14:51:30 +0200

    feat(factory): add the feat for cylinder (#45)

--- src/RayTracer/Factory/Factory.hpp
+++ src/RayTracer/Factory/Factory.hpp
@@ -15,6 +15,7 @@
 #include <functional>
 #include "Sphere.hpp"
 #include "Plane.hpp"
+#include "Cylindre.hpp"
 
 namespace RayTracer {
     class Factory {
@@ -30,6 +31,7 @@ namespace RayTracer {
             std::map<std::string, std::function<std::shared_ptr<RayTracer::IShape>(const std::string&)>> _shapeMap = {
                {"Sphere", [](const std::string& name){ return std::make_shared<RayTracer::Sphere>(name);}},
                {"Plane", [](const std::string& name){ return std::make_shared<RayTracer::Plane>(name);}},
+               {"Cylindre", [](const std::string& name){ return std::make_shared<RayTracer::Cylindre>(name);}},
             };
             std::map<std::string, std::function<std::shared_ptr<RayTracer::ILight>(const std::string&)>> _lightMap = {
             };
