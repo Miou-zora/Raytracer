0b70fe1d4163d75330670742996f629275ac3578
miouzora <alexandre.franquet@epitech.eu>
2023-05-11 23:07:51 +0200

    refacto(Core): remove useless include

--- src/Core.cpp
+++ src/Core.cpp
@@ -7,17 +7,6 @@
 
 #include "Core.hpp"
 
-#include <iostream>
-#include "Sphere.hpp"
-#include "Plane.hpp"
-#include "Camera.hpp"
-#include "EnhanceRenderer.hpp"
-#include "FastRenderer.hpp"
-#include "PPMDisplayer.hpp"
-#include "DirectionalLight.hpp"
-#include "Cylinder.hpp"
-#include <cmath>
-
 void RayTracer::Core::run(void)
 {
 
