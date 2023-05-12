e30f67aa3a183aa04ee24a391de3ab0029840b2f
Timothée Lesellier <timothee.lesellier@epitech.eu>
2023-05-12 15:43:46 +0200

    refactor(core): removed the 'build' part of the core to the class 'Builder'

--- src/Core.hpp
+++ src/Core.hpp
@@ -31,18 +31,6 @@ namespace RayTracer {
             RayTracer::Scene getScene(void) const { return _scene; };
         private:
             RayTracer::Scene _scene;
-        //     void setScenePath(std::string scenePath);
-        //     std::shared_ptr<RayTracer::Scene> getScene(void) const;
-
-        // private:
-
-        //     void initCamera(libconfig::Setting &cameraSetting);
-        //     void setTransformation(libconfig::Setting &setting, RayTracer::Camera &camera);
-        //     void initPrimitives(libconfig::Setting &setting);
-        //     void initLights(libconfig::Setting &setting);
-
-        //     std::string _scenePath;
-        //     std::shared_ptr<RayTracer::Scene> _scene;
             std::shared_ptr<RayTracer::IRenderer> _renderer;
             std::shared_ptr<RayTracer::IDisplayer> _displayer;
     };
