15c4524917db1940c2cfc67b4f82881937bf1645
Timothée Lesellier <timothee.lesellier@epitech.eu>
2023-05-13 22:54:56 +0200

    refactor(Cylinder): custom ctor

--- src/RayTracer/Object/Shape/Cylinder/Cylinder.cpp
+++ src/RayTracer/Object/Shape/Cylinder/Cylinder.cpp
@@ -103,5 +103,7 @@ RayTracer::Cylinder::Cylinder(libconfig::Setting &setting)
     setRotation(Convertissor.ToVertex(setting, "rotation"));
     setMaterial(Convertissor.ToMaterial(setting, "material"));
     translate((double)translation._x, (double)translation._y, (double)translation._z);
-    _radius = 1;
+    _radius = Convertissor.get<double>(setting, "radius", 1.0);
+    std::cerr << "Cylinder created" << std::endl;
+    std::cerr << "Position: " << getPosition()._x << " " << getPosition()._y << " " << getPosition()._z << std::endl;
 }
