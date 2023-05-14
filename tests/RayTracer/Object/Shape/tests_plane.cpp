6e901e1116bd26cf12e2b60f73de8ba2f7fc4286
queng <quentin.brejoin@gmail.com>
2023-05-14 13:23:25 +0200

    feat(core): add tests

--- tests/RayTracer/Object/Shape/tests_plane.cpp
+++ tests/RayTracer/Object/Shape/tests_plane.cpp
@@ -9,34 +9,34 @@
 
 #include "Plane.hpp"
 
-// Test(plane, dontHit) {
-//     RayTracer::Plane plane;
-//     Maths::Ray ray(Maths::Vertex(0, 0, 1), Maths::Vector(0, 1, 0));
+Test(plane, dontHit) {
+    RayTracer::Plane plane;
+    Maths::Ray ray(Maths::Vertex(0, 0, 1), Maths::Vector(0, 1, 0));
 
-//     RayTracer::HitRecord hit = plane.hit(ray);
-//     cr_assert(hit.isHit() == false);
-// }
+    RayTracer::HitRecord hit = plane.hit(ray);
+    cr_assert(hit.isHit() == false);
+}
 
-// Test(plane, hitFrontFace) {
-//     RayTracer::Plane plane(Maths::Vertex(0, 0, 0));
-//     Maths::Ray ray(Maths::Vertex(0, 0, 1), Maths::Vector(0, 0, -1));
+Test(plane, hitFrontFace) {
+    RayTracer::Plane plane(Maths::Vertex(0, 0, 0));
+    Maths::Ray ray(Maths::Vertex(0, 0, 1), Maths::Vector(0, 0, -1));
 
-//     RayTracer::HitRecord hit = plane.hit(ray);
-//     cr_assert(hit.isHit());
-//     cr_assert(hit.getMaterial() == plane.getMaterial());
-//     cr_assert(hit.getNormal() == Maths::Vector(0, 0, 1));
-//     cr_assert(hit.getIntersectionPoint() == Maths::Vertex(0, 0, 0));
-//     cr_assert(hit.isFrontFace() == true);
-// }
+    RayTracer::HitRecord hit = plane.hit(ray);
+    cr_assert(hit.isHit());
+    cr_assert(hit.getMaterial() == plane.getMaterial());
+    cr_assert(hit.getNormal() == Maths::Vector(0, 0, 1));
+    cr_assert(hit.getIntersectionPoint() == Maths::Vertex(0, 0, 0));
+    cr_assert(hit.isFrontFace() == true);
+}
 
-// Test(plane, hitBackFace) {
-//     RayTracer::Plane plane(Maths::Vertex(0, 0, 0));
-//     Maths::Ray ray(Maths::Vertex(0, 0, -1), Maths::Vector(0, 0, 1));
+Test(plane, hitBackFace) {
+    RayTracer::Plane plane(Maths::Vertex(0, 0, 0));
+    Maths::Ray ray(Maths::Vertex(0, 0, -1), Maths::Vector(0, 0, 1));
 
-//     RayTracer::HitRecord hit = plane.hit(ray);
-//     cr_assert(hit.isHit());
-//     cr_assert(hit.getMaterial() == plane.getMaterial());
-//     cr_assert(hit.getNormal() == Maths::Vector(0, 0, -1));
-//     cr_assert(hit.getIntersectionPoint() == Maths::Vertex(0, 0, 0));
-//     cr_assert(hit.isFrontFace() == false);
-// }
+    RayTracer::HitRecord hit = plane.hit(ray);
+    cr_assert(hit.isHit());
+    cr_assert(hit.getMaterial() == plane.getMaterial());
+    cr_assert(hit.getNormal() == Maths::Vector(0, 0, -1));
+    cr_assert(hit.getIntersectionPoint() == Maths::Vertex(0, 0, 0));
+    cr_assert(hit.isFrontFace() == false);
+}
